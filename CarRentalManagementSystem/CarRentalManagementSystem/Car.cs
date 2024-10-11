400ce14f503efd1886f9850e380f73447a4ff5f9
Pandiyaraj S. <UT01117@unicomtic.com>
2024-10-11 14:07:39 +0530

    first commit

---
+++ CarRentalManagementSystem/CarRentalManagementSystem/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalManagementSystem
+{
+    internal class Car
+    {
+        private int CarId { get; set; }
+        private string Brand { get; set; }
+        private string Model { get; set; }
+        private decimal RentalPrice { get; set; }
+
+        public Car(int carId, string brand, string model, decimal rentalPrice)
+        {
+            CarId = carId;
+            Brand = brand;
+            Model = model;
+            RentalPrice = rentalPrice;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {CarId}, Brand: {Brand}, Model: {Model}, RentalPrice: {RentalPrice}";
+        }
+    }
+}
